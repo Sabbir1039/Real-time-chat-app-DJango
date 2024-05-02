de7cee0ebfa6add955ea061232e2c5be55b6c165
Sabbir Hossain <cse.170201039@gmail.com>
2024-05-03 01:01:04 +0600

    core project files added

---
+++ core/urls.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('users/', include('usersapp.urls')),
+    path('chat/', include('chatapp.urls')),
+    
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

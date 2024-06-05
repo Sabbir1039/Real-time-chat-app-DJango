a023ae1d135a44889e6003e1c5cf9bdf9ca94fbc
Sabbir Hossain <cse.170201039@gmail.com>
2024-06-05 21:49:23 +0600

    explanation of code added as comments

--- chatapp/consumers.py
+++ chatapp/consumers.py
@@ -6,20 +6,24 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 class ChatConsumer(AsyncWebsocketConsumer):
     
     async def connect(self):
-        self.room_name = self.scope['url_route']['kwargs']['room_name'] # extracts the room_name from the URL route parameters
-        self.room_group_name = 'chat_%s' % self.room_name
+        # extracts the room_name from the URL route parameters
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        # Creating a unique group name for the room
+        self.room_group_name = f'chat_{self.room_name}'
         
         print("*****Connected")
         
+        # Add the channel (WebSocket connection) to the group
         await self.channel_layer.group_add(
             self.room_group_name,
-            self.channel_name
+            self.channel_name # Unique identifier for the WebSocket connection
         )
-        
+        # Accept the WebSocket connection
         await self.accept()
         
 
     async def disconnect(self, close_code):
+        # Remove the channel from the group on disconnect
         print("*****disconnected")
         await self.channel_layer.group_discard(
             self.room_group_name,
@@ -27,10 +31,12 @@ class ChatConsumer(AsyncWebsocketConsumer):
         )
 
     async def receive(self, text_data):
+        # Receive a message from the WebSocket
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
         username = text_data_json["username"]
 
+        # Send the message to the group
         await self.channel_layer.group_send(
             self.room_group_name,
             {

7e3b071e95db19a0668f5e5151152058ec83fb56
Sabbir Hossain <cse.170201039@gmail.com>
2024-07-18 01:33:42 +0600

    setting updated for static files

--- core/settings.py
+++ core/settings.py
@@ -127,6 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+# Directory where collectstatic will gather static files for deployment
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

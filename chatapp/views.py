d6c32ce813ef98abf49995e69bfcb43542c2d2c4
Sabbir Hossain <cse.170201039@gmail.com>
2024-07-18 03:19:27 +0600

    messages extract from database and added to frontend

--- chatapp/views.py
+++ chatapp/views.py
@@ -1,12 +1,26 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import TemplateView
+from .models import Room, Message
 
 def index(request):
     user = request.user
     if user.is_authenticated:
-        return render(request, "chatapp/room-entry.html")
+        rooms = Room.objects.all().order_by('-created_at')
+        context = {
+            "rooms": rooms
+        }
+        return render(request, "chatapp/room-entry.html", context)
     else:
         return redirect('login')
 
 def room(request, room_name):
-    return render(request, "chatapp/room.html", {"room_name": room_name})
+    room = Room.objects.filter(name=room_name).first()
+    if not room:
+        messages = None   
+    else:
+        messages = Message.objects.filter(room=room).order_by('timestamp')
+    
+    return render(request, "chatapp/room.html", {
+        "room_name": room_name,
+        "messages": messages
+    })

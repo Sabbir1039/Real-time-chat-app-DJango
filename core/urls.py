cb7ca83b03171dec97a530f4331e93c1f7839912
Md. Sabbir Hossain <cse.170201039@gmail.com>
2024-08-26 17:39:56 +0600

    handler404 method added for custom_404 page

--- core/urls.py
+++ core/urls.py
@@ -2,10 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('usersapp.urls')),
-    path('chat/', include('chatapp.urls')),
-    
+    path('chat/', include('chatapp.urls')),  
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = "chatapp.views.custom_404"

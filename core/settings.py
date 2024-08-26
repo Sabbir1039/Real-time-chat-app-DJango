01f0b7d3d7548c63184ccb680071f5d9afbfa504
Md. Sabbir Hossain <cse.170201039@gmail.com>
2024-08-26 17:37:20 +0600

    set debug=Flase and added allowed host to test custom_404 page

--- core/settings.py
+++ core/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7tc7*31ok2@-5s*ghf2cljq(+1fyd!)d8+iv@45km#fw%o17z_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
